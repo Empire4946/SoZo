9ae4b9593c8175abb12ec75ba2672afcf1c5724c
Empire <104152235+Empire4946@users.noreply.github.com>
2022-07-07 19:05:37 +0800

    update script

---
+++ SoZo/loadstring.lua
@@ -0,0 +1,17 @@
+--[[
+███████╗ ██████╗ ███████╗ ██████╗    
+██╔════╝██╔═══██╗╚══███╔╝██╔═══██╗   
+███████╗██║   ██║  ███╔╝ ██║   ██║   
+╚════██║██║   ██║ ███╔╝  ██║   ██║   
+███████║╚██████╔╝███████╗╚██████╔╝██╗
+╚══════╝ ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝
+ 
+Public Dev
+ver;1.1
+
+Updates:
+StreamProof
+
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Empire4946/SoZo/main/SoZo/script.txt",true))()
