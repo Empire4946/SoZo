78f32abe56b0cb0f3b0a61af3b3e741a2c903de5
Empire <104152235+Empire4946@users.noreply.github.com>
2022-07-07 22:20:36 +0800

    Update loadstring.lua

--- SoZo/loadstring.lua
+++ SoZo/loadstring.lua
@@ -14,4 +14,17 @@ StreamProof
 
 ]]
 
+local gamemt = getrawmetatable(game)
+setreadonly(gamemt, false)
+local nc = gamemt.__namecall
+
+gamemt.__namecall = newcclosure(function(...)
+  if (getnamecallmethod() == 'GetTotalMemoryUsageMb') then
+    return math.random(395, 405)
+  end
+  return nc(...)
+end)
+
+hookfunction(game.Stats.GetTotalMemoryUsageMb, function() return math.random(395, 405) end) -- Memory Spoofer
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Empire4946/SoZo/main/SoZo/script.txt",true))()
